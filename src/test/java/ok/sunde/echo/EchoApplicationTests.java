11d868e827798bea3be9f4cd0e3ab97b6815a767
Ole Kristian Sunde <ole.kristian.sunde@vps.no>
2018-01-08 14:55:57 +0100

    Removes server.context-path (not needed)

--- src/test/java/ok/sunde/echo/EchoApplicationTests.java
+++ src/test/java/ok/sunde/echo/EchoApplicationTests.java
@@ -52,7 +52,7 @@ public class EchoApplicationTests {
     }
 
     private String url() {
-        return format("http://localhost:%d/echo/", port);
+        return format("http://localhost:%d/", port);
     }
 
 }
